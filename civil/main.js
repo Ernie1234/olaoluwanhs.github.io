33d53cdb9876b8ad307ae4033b45c4bcb49176aa
olaoluwanhs <63165776+olaoluwanhs@users.noreply.github.com>
2021-05-01 18:09:26 +0100

    Add files via upload

--- civil/main.js
+++ civil/main.js
@@ -3,13 +3,14 @@ const left = document.getElementById('left');
 const carousel = document.querySelector('.carousel');
 
 let counter = 1;
-right.addEventListener('click', () => {
+function forward() {
     if (counter === 3) {
         counter = 0;
     }
     carousel.style.transform = `translateX(-${counter * 100}vw)`;
     counter = counter + 1;
-});
+}
+right.addEventListener('click', forward);
 
 var counterBack;
 left.addEventListener('click', () => {
@@ -23,3 +24,4 @@ left.addEventListener('click', () => {
     carousel.style.transform = `translateX(-${counter * 100}vw)`;
 
 });
+setInterval(forward, 4000)
